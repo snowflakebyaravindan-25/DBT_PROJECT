1c779d377b917e4f59d10ec069b5146d2f3d0236
snowflakebyaravindan-25 <aravindans2503@gmail.com>
2025-09-19 07:53:01 +0000

    Added Generate Schema

---
+++ taco_bytes/macros/generate_schema_name.sql
@@ -0,0 +1,7 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+    {% if target.name == 'prod' %}
+        {{ custom_schema_name }}
+    {% else %}
+        {{ target.schema }}_{{ custom_schema_name }}
+    {% endif %}
+{% endmacro %}
